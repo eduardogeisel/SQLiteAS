58ded066ebb2dc37d1355457a260cf77f85d394a
Eduardo Peres <eduardogeisel@hotmail.com>
2021-03-16 16:48:16 -0700

    Created model class with constructors, getters, setters and toString

--- app/src/main/java/com/example/sqlitetutorial/TeamModel.java
+++ app/src/main/java/com/example/sqlitetutorial/TeamModel.java
@@ -1,4 +1,54 @@
 package com.example.sqlitetutorial;
 
 public class TeamModel {
+
+    private int id;
+    private String teamName;
+    private int yearFounded;
+
+    //constructors
+    public TeamModel() {
+        //non parameterized constructor
+    }
+
+    public TeamModel(int id, String teamName, int yearFounded) {
+        this.id = id;
+        this.teamName = teamName;
+        this.yearFounded = yearFounded;
+    }
+
+    //toString is necessary for printing the contents of a class object
+    @Override
+    public String toString() {
+        return "TeamModel{" +
+                "id=" + id +
+                ", teamName='" + teamName + '\'' +
+                ", yearFounded=" + yearFounded +
+                '}';
+    }
+
+    //getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public int getYearFounded() {
+        return yearFounded;
+    }
+
+    public void setYearFounded(int yearFounded) {
+        this.yearFounded = yearFounded;
+    }
 }
