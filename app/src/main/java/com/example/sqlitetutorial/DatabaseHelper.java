82ac0a9b3e6d0c98030ba42f911b486dc8b88753
Eduardo Peres <eduardogeisel@hotmail.com>
2021-03-16 18:56:36 -0700

    Data created and appearing on list view

--- app/src/main/java/com/example/sqlitetutorial/DatabaseHelper.java
+++ app/src/main/java/com/example/sqlitetutorial/DatabaseHelper.java
@@ -1,4 +1,89 @@
 package com.example.sqlitetutorial;
 
-public class DatabaseHelper {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String TEAM_TABLE = "TEAM_TABLE";
+    public static final String COLUMN_TEAM_NAME = "TEAM_NAME";
+    public static final String COLUMN_TEAM_SINCE = "TEAM_SINCE";
+    public static final String COLUMN_ID = "ID";
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, "teams.db", null, 1);
+    }
+
+    //this is called the first time a database is accessed. There should be code here to create a new database
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTableStatement = "CREATE TABLE " + TEAM_TABLE + " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COLUMN_TEAM_NAME + " TEXT, " + COLUMN_TEAM_SINCE + " INT)";
+
+        db.execSQL(createTableStatement);
+    }
+
+    //this is called if the database version number changes
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public boolean addOne (TeamModel teamModel){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_TEAM_NAME, teamModel.getTeamName());
+        cv.put(COLUMN_TEAM_SINCE, teamModel.getYearFounded());
+
+        long insert = db.insert(TEAM_TABLE, null, cv);
+        if (insert == -1){
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    public List<TeamModel> getAllTeams(){
+
+        List<TeamModel> returnList = new ArrayList<>();
+
+        String queryStr = "SELECT * FROM " + TEAM_TABLE;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery(queryStr, null);
+
+        if(cursor.moveToFirst()) {
+            do {
+                int teamID = cursor.getInt(0);
+                String teamName = cursor.getString(1);
+                int yearFounded = cursor.getInt(2);
+
+                TeamModel newTeam = new TeamModel(teamID, teamName, yearFounded);
+                returnList.add(newTeam);
+
+            } while (cursor.moveToNext());
+
+        }
+        else {
+            //failed, do nothing
+        }
+
+        //close both cursor and db
+        cursor.close();
+        db.close();
+
+        return returnList;
+    }
 }
--- app/src/main/java/com/example/sqlitetutorial/MainActivity.java
+++ app/src/main/java/com/example/sqlitetutorial/MainActivity.java
@@ -4,15 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText etTeamName, etTeamYear;
     Button btnAdd, btnView;
     ListView lvTeams;
+    ArrayAdapter teamArrayAdapter;
+    DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +31,28 @@ public class MainActivity extends AppCompatActivity {
         etTeamName = findViewById(R.id.etTeamName);
         etTeamYear = findViewById(R.id.etTeamYear);
 
+        databaseHelper = new DatabaseHelper(MainActivity.this);
+
         //click listeners
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TeamModel teamModel = new TeamModel(-1, etTeamName.getText().toString(), Integer.parseInt(etTeamYear.getText().toString()));
+
+                TeamModel teamModel;
+
+                try{
+                   teamModel = new TeamModel(-1, etTeamName.getText().toString(), Integer.parseInt(etTeamYear.getText().toString()));
+                } catch (Exception e) {
+                    Toast.makeText(MainActivity.this, "Error adding team", Toast.LENGTH_SHORT).show();
+                    teamModel = new TeamModel(-1, "error", 0);
+                }
+
+                DatabaseHelper databaseHelper = new DatabaseHelper(MainActivity.this);
+
+                databaseHelper.addOne(teamModel);
+
+                ShowTeamOnListView(databaseHelper);
+
             }
         });
 
@@ -37,7 +60,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                databaseHelper = new DatabaseHelper(MainActivity.this);
+                List<TeamModel> allTeams = databaseHelper.getAllTeams();
+
+                ShowTeamOnListView(databaseHelper);
             }
         });
+
+        //Calling these inside onCreate to show list as soon as app is opened
+        ShowTeamOnListView(databaseHelper);
+    }
+
+    private void ShowTeamOnListView(DatabaseHelper databaseHelper) {
+        teamArrayAdapter = new ArrayAdapter<TeamModel>(MainActivity.this, android.R.layout.simple_list_item_1, databaseHelper.getAllTeams());
+        lvTeams.setAdapter(teamArrayAdapter);
     }
 }
