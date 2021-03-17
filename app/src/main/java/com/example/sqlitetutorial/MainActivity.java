ad2d177cc9d06972c53e11991b2dd097c64380aa
Eduardo Peres <eduardogeisel@hotmail.com>
2021-03-16 17:05:19 -0700

    Added values to resoures

--- app/src/main/java/com/example/sqlitetutorial/MainActivity.java
+++ app/src/main/java/com/example/sqlitetutorial/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.sqlitetutorial;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etTeamName, etTeamYear;
+    Button btnAdd, btnView;
+    ListView lvTeams;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAdd = findViewById(R.id.btnAdd);
+        btnView = findViewById(R.id.btnView);
+        lvTeams = findViewById(R.id.lvTeams);
+        etTeamName = findViewById(R.id.etTeamName);
+        etTeamYear = findViewById(R.id.etTeamYear);
+
+        //click listeners
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TeamModel teamModel = new TeamModel(-1, etTeamName.getText().toString(), Integer.parseInt(etTeamYear.getText().toString()));
+            }
+        });
+
+        btnView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
